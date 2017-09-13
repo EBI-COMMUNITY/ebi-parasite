7912b623b80a2fd527401758f7d3183b7fa0c7e8
nimapak <nim.pak@gmail.com>
2017-09-13 15:14:16 +0100

    Update quality-control.py
    Updated for Xin

--- quality-control.py
+++ quality-control.py
@@ -1,4 +1,6 @@
 import argparse
+from utilities import command
+from utilities import fileutils
 
 
 
@@ -24,27 +26,65 @@ def get_args():
     print "fastq1:",fastq1
     print "fastq2:",fastq2
 
+   
+def run_trim_galore(fastqfiles):
+        
+        #pair fastq files
+		if len(fastqfiles)==2:
+			comm=self.prop.trim_galore+" --paired -q 20 "+ fastqfiles[0]+" "+fastqfiles[1]
+        #single fastq files    
+		elif len(fastqfiles)==2:
+			comm=self.prop.trim_galore+" -q 20 "+ fastqfiles[0]
+        else
+            print "unkown number of fq files."
+		print comm
+		comm_obj=command(comm)
+		returncode, stdout, stderr=comm_obj.run(3600)
+		print returncode, stdout, stderr
+		print self.prop.workdir
+		
 
 
 def initiate():
     #Here please provide the initiation code
      print "Here please do the initiation code"
+     #fi=fileutils()
+	 #indir=self.prop.workdir+"/quality/in/"
+     #outdir=self.prop.workdir+"/quality/out/"
+	 #fi.create_processing_dir(indir)
+	 #fi.create_processing_dir(outdir)
+	 #fi.copy_src_into_dest(self.fq1,indir)
+	 #fi.copy_src_into_dest(self.fq2,indir)
 
 
 def execute():
     #Here please provide the execucation code
     print "Here please do the execucation  code"
+    run_trim_galore()
     
     #Execution of the external softwares, you need to use command class from utilities
     #Example: from utilities import command
     #Then you can create an object of command class and then use a function:
     #Example:
-    #command = Command("python /usr/bin/spades.py -1 8605_7_1_test.fastq -2 8605_7_2_test.fastq -k 21,33,55,77,99,127 --careful --only-assembler -o sp    ades_small_dir")
+    #comm = command("python /usr/bin/spades.py -1 8605_7_1_test.fastq -2 8605_7_2_test.fastq -k 21,33,55,77,99,127 --careful --only-assembler -o sp    ades_small_dir")
     #returncode,stdout,stderr=command.run(timeout=5)
 
 def post_process():
     #Here please provide the post process code
     print "Here please do the post process  code"
+    	fi=fileutils()
+		indir=self.prop.workdir+"/quality/in/"
+		outdir=self.prop.workdir+"/quality/out/"
+		fi.create_processing_dir(indir)
+		fi.create_processing_dir(outdir)
+		fi.copy_src_into_dest(self.fq1,indir)
+		fi.copy_src_into_dest(self.fq2,indir)
+		fqout1=self.fq1+"_val_1.fq"
+		fqout2=self.fq2+"_val_2.fq"
+		report=self.fq1+"_trimming_report.txt"
+		fi.copy_src_into_dest(fqout1,indir)
+		fi.copy_src_into_dest(fqout2,indir)
+		fi.copy_src_into_dest(report,indir)
 
 
 
