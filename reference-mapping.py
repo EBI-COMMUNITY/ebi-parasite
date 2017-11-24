bb9460e1b49f41e9099e8581520fc306aceaf8f1
xinliu005 <31921475+xinliu005@users.noreply.github.com>
2017-11-24 15:52:17 +0000

    Add files via upload

--- reference-mapping.py
+++ reference-mapping.py
@@ -9,9 +9,9 @@ from utilities import properties
 
 
 def get_args():
-
+    
+    global fi
     global properties_file
-    global mapping_software
     global genome_name
     global ref_fasta
     global fastq1
@@ -19,77 +19,71 @@ def get_args():
     global prefix
     global ref_fasta_root_default
     
+    fi=fileutils()
     ref_fasta_root_default='ena_ref_fasta'
-    
     # Assign description to the help doc
-    parser = argparse.ArgumentParser(description='Script mapping short reads to reference genomes using bwa')
+    parser = argparse.ArgumentParser(description='Script mapping short reads to reference genomes using BWA')
     parser.add_argument('-p', '--properties_file', type=str, help='Please provide the properties file', required=True)
-    parser.add_argument('-g', '--genome_name', type=str, help='''Please provide the genome name you are mapping to, only \'ch\' or \'cp\' permitted, 
-                                                                \'ch\' stands for \'Cryptosporidium% hominis\' and 
-                                                                \'cp\' stands for \'Cryptosporidium% parvum\', required=True''', required=True)
+    parser.add_argument('-g', '--genome_name', type=str, help='''Please provide the genome name you are mapping to, 
+                        only \'ch\' or \'cp\' permitted.
+                        \'ch\' stands for \'Cryptosporidium hominis\' and \'cp\' stands for \'Cryptosporidium parvum\'''',
+                        required=True)
     parser.add_argument('-r', '--ref_fasta', type=str, help='Please provide reference genome fasta file', required=False)
     parser.add_argument('-fq1', '--fastq1', type=str, help='Please provide the forward fastq file', required=True)
     parser.add_argument('-fq2', '--fastq2', type=str, help='Please provide the reverse fastq file', required=False)
-    parser.add_argument('-pre', '--prefix', type=str, help='Please provide the prefix for the output file', 
-        required=True)  
+    parser.add_argument('-pre', '--prefix', type=str, help='Please provide the prefix for the output file', required=True)  
     
-    mapping_software='bwa'
     args=parser.parse_args()
+    # check args
+    fi.check_files_exist([args.properties_file, args.fastq1])   
+    if args.genome_name!='ch' and genome_name!='cp':
+        print "ERROR: Please provide the genome name you are mapping to, only \'ch\' or \'cp\' permitted"
+        sys.exit(1)    
+    if args.ref_fasta is not None:
+        fi.check_file_exist(args.ref_fasta)
+        
+    # define variables        
     properties_file=args.properties_file
     genome_name=args.genome_name 
-    ref_fasta=args.ref_fasta
-    if ref_fasta is None:
+    if args.ref_fasta is None:
         ref_fasta=ref_fasta_root_default+'_'+genome_name
     fastq1=args.fastq1
     fastq2=args.fastq2  
     prefix=args.prefix    
-    
-    if genome_name!='ch' and genome_name!='cp':
-        print "ERROR: Please provide the genome name you are mapping to, only \'ch\' or \'cp\' permitted"
-        sys.exit(1)
         
     print "properties_file:",properties_file  
-    print "mapping_software:",mapping_software
     print "genome_name:",genome_name
     print "refrence_genome fasta file:",ref_fasta
     print "fastq1:",fastq1
     print "fastq2:",fastq2
     print "prefix:",prefix
-
-
-def get_property(key):
-    try:
-        return getattr(prop, key) 
-    except AttributeError:
-        print "ERROR: {} path was not defined in {}. Please define it and re-run.".format(key,properties_file)        
-        sys.exit(1)
-
-    
-def run_mapping(fastqfiles):            
+  
+   
+def run_mapping(fastqfiles):  
+    global ref_fasta          
     fi.change_dir(workdir)     
-    bwa_path=get_property(mapping_software)
-    ref_fasta_path=get_property(ref_fasta)
-        
+    bwa_path=getattr(prop,"bwa")
+    if ref_fasta == "{}_{}".format(ref_fasta_root_default,genome_name):        
+        ref_fasta=getattr(prop,ref_fasta)
+    
     #define command         
     fq1=fastqfiles[0]
     fq1_sai=out_prefix+reads_type+'.fq1.sai'
+    comm="{} aln -t 12 {} {} >{}; ".format(bwa_path,ref_fasta,fq1,fq1_sai)
     if len(fastqfiles)==2:
         fq2=fastqfiles[1]
-        fq2_sai=out_prefix+reads_type+'.fq2.sai'
-        comm="{} aln {} {} >{}; ".format(bwa_path,ref_fasta_path,fq1,fq1_sai)
-        comm+=("{} aln {} {} >{}; ".format(bwa_path,ref_fasta_path,fq2,fq2_sai))
-        comm+=("{} sampe {} {} {} {} {}>{}{}.sam".format(bwa_path,ref_fasta_path,fq1_sai,fq2_sai,fq1,fq2,out_prefix,reads_type))
+        fq2_sai=out_prefix+reads_type+'.fq2.sai'        
+        comm+="{} aln -t 12 {} {} >{}; ".format(bwa_path,ref_fasta,fq2,fq2_sai)
+        comm+="{} sampe {} {} {} {} {}>{}{}.sam".format(bwa_path,ref_fasta,fq1_sai,fq2_sai,fq1,fq2,out_prefix,reads_type)
     elif len(fastqfiles)==1:
-        comm="{} aln {} {}>{}; ".format(bwa_path,ref_fasta_path,fq1,fq1_sai)
-        comm+=("{} samse {} {} {}>{}{}.sam".format(bwa_path,ref_fasta_path,fq1_sai,fq1,out_prefix,reads_type)) 
+        comm+="{} samse {} {} {}>{}{}.sam".format(bwa_path,ref_fasta,fq1_sai,fq1,out_prefix,reads_type)
         
     #run command  
-    returncode, stdout, stderr=command(comm).run(3600)
+    returncode, stdout, stderr=command(comm).run(360000)
     if returncode!=0:
         print "\nERROR: {} failed!!! \n\nSTDERR: {}\nSTDOUT: {}\n".format(comm,stderr,stdout)    
         sys.exit(1) 
     print returncode, stdout, stderr
-    print prop.workdir
 
 
 def initiate():
@@ -115,7 +109,7 @@ def initiate():
     if fastq2 is not None:
         reads_type='paired'
         fi.copy_file_into_dest(fastq2,indir)
-    out_prefix="{}_{}_{}_".format(prefix.upper(),mapping_software,genome_name)
+    out_prefix="{}_{}_".format(prefix.upper(),genome_name)
     
 
 def execute():
